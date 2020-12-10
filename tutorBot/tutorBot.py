d2299544edb375e0ae2ee67ae1a94823c8069f0c
n111h <noh@groot.localdomain>
2020-12-10 14:12:46 -0500

    added scripts

--- tutorBot/tutorBot.py
+++ tutorBot/tutorBot.py
@@ -18,6 +18,8 @@ from slackeventsapi import SlackEventAdapter
 token = ""
 secret = ""
 responses = ["Hey!", "Hi!", "What's up?", "Does this syntax make me look fat?", "That's Tudor to you!", "Genaric response"]
+block = '{"text": "Question text","fallback":"Question text fallback","callback_id":"question_id","attachment_type":"default",'
+block += '"actions":[{"name":"ans","text":"A","type":"button","value":"a"}{"name":"ans","text":"B","type":"button","value":"b"}'
 
 ####  FUNCTIONS  ###############################################################################################################
 
@@ -31,14 +33,35 @@ botID = bot.api_call("auth.test")["user_id"]                    #  save bot id s
 @slackEvent.on("message")                                       #  message.channels event ("event"{"type":"message"})
 def message(content):
     event = content.get("event", {})
+    print(content,event,sep='\n')                               #  print entire event (use for log (pipe))
+
     channel = event.get("channel")
     user = event.get("user")
-    text = event.get("text")
-
-    print(content)                                              #  print entire event (use for log (pipe))
 
     if(user != botID):                                          #  post a genaric message
-        bot.chat_postMessage(channel=channel,text=responses[int(random()*len(responses))])
+        bot.chat_postMessage(channel=channel,text=(responses[int(random()*len(responses))]),)
+            
+
+@slackEvent.on("app_mention")
+def mention(content):
+    event = content.get("event", {})
+    print(content,event,sep='\n')
+
+    channel = event.get("channel")
+    user = event.get("user")
+
+    bot.chat_postEphemeral(channel=channel,text="this is a response to a message",user=user,blocks=block)
+
+
+@slackEvent.on("message.app_home")
+def privateMessage(content):
+    event = content.get("event", {})
+    print(content,event,sep='\n')
+
+    channel = event.get("channel")
+    user = event.get("user")
+
+    bot.chat_postEphemeral(channel=channel,text="this is a response to a message",user=user,blocks=block)
 
 
 if(__name__=="__main__"): server.run()                          #  might need to change debug to False
